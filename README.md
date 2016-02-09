b02976a3958753869cedc1025a02fec1dec037f5
Rob Winch <rwinch@gopivotal.com>
2016-02-09 14:46:59 -0600

    Change ErrorViewWithoutPrincipalApplicationTests to integration tests

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 We would like to include the current user's name in the error template view. If the user is not logged in, we may want to include a link to login or nothing. But it turns out the error view has no access to the Spring Authentication object of the current user.
 
 Steps to reproduce the issue:
-<ul><li>Start up the spring boot app by running ErrorViewWithoutPrincipalApplication</li>
+<ul>
 <li>Execute test case ErrorViewWithoutPrincipalApplicationTests: 2 test cases will pass and 1 will fail because it cannot find the logged in user who is authenticated but has no the required role.</li>
 </ul>
 
--- src/test/java/io/pivotal/demo/ErrorViewWithoutPrincipalApplicationTests.java
+++ src/test/java/io/pivotal/demo/ErrorViewWithoutPrincipalApplicationTests.java
@@ -7,8 +7,11 @@ import java.util.Collections;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -16,49 +19,55 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ErrorViewWithoutPrincipalApplication.class)
-@WebAppConfiguration
+@WebIntegrationTest(randomPort = true)
 public class ErrorViewWithoutPrincipalApplicationTests {
 
 	RestTemplate template;
-	
 
-	
+	String host;
+
+	@Autowired
+	public void setPort(@Value("${local.server.port}") int port) {
+		this.host = "http://localhost:"+port;
+	}
+
+
+
 	@Test
 	public void tryOpenWithoutCredentials() {
 		template = new TestRestTemplate();
-		
-		ResponseEntity<String> response = template.getForEntity("http://localhost:8080/open", String.class);
+
+		ResponseEntity<String> response = template.getForEntity(host + "/open", String.class);
 		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
 		System.out.println(response.getBody());
 	}
 
 
-	
+
 	@Test
 	public void tryOpenWithCredentials() {
 		template = new TestRestTemplate("bob", "pwd");
-		
-		ResponseEntity<String> response = template.getForEntity("http://localhost:8080/open", String.class);
+
+		ResponseEntity<String> response = template.getForEntity(host + "/open", String.class);
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		System.out.println(response.getBody());
 		assertTrue(response.getBody().contains("bob"));
 	}
-	
+
 	@Test
 	public void tryCloseWithCredentialsButWithoutRequiredRole() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Collections.singletonList(MediaType.TEXT_HTML));
 		template = new TestRestTemplate("bob", "pwd");
-		
 
-		ResponseEntity<String> response = template.exchange("http://localhost:8080/close", HttpMethod.GET, new HttpEntity<String>(headers), String.class);
+
+		ResponseEntity<String> response = template.exchange(host + "/close", HttpMethod.GET, new HttpEntity<String>(headers), String.class);
 		System.out.println(response.getBody());
-		
+
 		assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
 		assertTrue(response.getBody().contains("bob"));
 	}
